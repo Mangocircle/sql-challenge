cf767c5fc2f0375eb3102ce6964320d468af0d72
Mangocircle <125223177+Mangocircle@users.noreply.github.com>
2023-04-24 23:15:54 -0600

    Add files via upload

--- sql_challenge/Data_Analysis.sql
+++ sql_challenge/Data_Analysis.sql
@@ -8,32 +8,32 @@ select e.first_name, e.last_name, e.hire_date
 from employees as e
 where hire_date between '1986-01-01' and '1986-12-31' ;
 
-SELECT d.dep_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+select d.dep_no, d.dept_name, e.emp_no, e.last_name, e.first_name
 from departments d
 join dept_manager dm
 	on dm.dept = d.dep_no
 join employees e
 	on e.emp_no = dm.emp_no;
 
-SELECT d.dep_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+select d.dep_no, d.dept_name, e.emp_no, e.last_name, e.first_name
 from departments d
 join dept_emp de
 	on de.dept_no = d.dep_no
 join employees e
 	on e.emp_no = de.emp_no;
 	
-SELECT first_name, last_name, sex
-FROM EMPLOYEES
-WHERE first_name = 'Hercules' 
-	AND last_name like 'B%';
+select first_name, last_name, sex
+from EMPLOYEES
+where first_name = 'Hercules' 
+	and last_name like 'B%';
 
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees e
-JOIN dept_emp de
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+join dept_emp de
 	on e.emp_no = de.emp_no
-JOIN departments d
+join departments d
 	on d.dep_no = de.dept_no
-WHERE
+where
 	d.dept_name = 'Sales'
 	OR d.dept_name = 'Development';
 
--- sql_challenge/Data_Engineering.sql
+++ sql_challenge/Data_Engineering.sql
@@ -1,72 +1,72 @@
-CREATE TABLE "titles" (
-    "title_id" varchar(30)   NOT NULL,
-    "title" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
+create table "titles" (
+    "title_id" varchar(30)   not null,
+    "title" varchar(30)   not null,
+    constraint "pk_titles" primary key (
         "title_id"
      )
 );
 
 
-CREATE TABLE "salaries" (
-    "emp_no" integer   NOT NULL,
-    "salary" integer   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
+create table "salaries" (
+    "emp_no" integer   not null,
+    "salary" integer   not null,
+    constraint "pk_salaries" primary key (
         "emp_no"
      )
 );
 
-CREATE TABLE "departments" (
-    "dep_no" varchar(30)   NOT NULL,
-    "dept_name" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
+create table "departments" (
+    "dep_no" varchar(30)   not null,
+    "dept_name" varchar(30)   not null,
+    constraint "pk_departments" primary key (
         "dep_no"
      )
 );
 
 
-CREATE TABLE "employees" (
-    "emp_no" integer   NOT NULL,
-    "emp_title_id" varchar(30)   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(30)   NOT NULL,
-    "last_name" varchar(30)   NOT NULL,
-    "sex" varchar(1)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
+create table "employees" (
+    "emp_no" integer   not null,
+    "emp_title_id" varchar(30)   not null,
+    "birth_date" date   not null,
+    "first_name" varchar(30)   not null,
+    "last_name" varchar(30)   not null,
+    "sex" varchar(1)   not null,
+    "hire_date" date   not null,
+    constraint "pk_employees" primary key (
         "emp_no"
      )
 );
 
-CREATE TABLE "dept_manager" (
-    "dept" varchar(30)   NOT NULL,
-    "emp_no" integer   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+create table "dept_manager" (
+    "dept" varchar(30)   not null,
+    "emp_no" integer   not null,
+    constraint "pk_dept_manager" primary key (
         "emp_no"
      )
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" integer   NOT NULL,
-    "dept_no" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+create table "dept_emp" (
+    "emp_no" integer   not null,
+    "dept_no" varchar(30)   not null,
+    constraint "pk_dept_emp" primary key (
         "emp_no", "dept_no"
      )
 );
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+alter table "salaries" add constraint "fk_salaries_emp_no" FOREIGN key("emp_no")
+references "employees" ("emp_no");
  
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
+alter table "employees" add constraint "fk_employees_emp_title_id" FOREIGN key("emp_title_id")
+references "titles" ("title_id");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept" FOREIGN KEY("dept")
-REFERENCES "departments" ("dep_no");
+alter table "dept_manager" add constraint "fk_dept_manager_dept" FOREIGN key("dept")
+references "departments" ("dep_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+alter table "dept_manager" add constraint "fk_dept_manager_emp_no" FOREIGN key("emp_no")
+references "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+alter table "dept_emp" add constraint "fk_dept_emp_emp_no" FOREIGN key("emp_no")
+references "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dep_no");
+alter table "dept_emp" add constraint "fk_dept_emp_dept_no" FOREIGN key("dept_no")
+references "departments" ("dep_no");
